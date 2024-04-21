5200060cbf30918af20189dda95667645962b0ba
Alishba Basharat <166277441+AlishbaBasharat@users.noreply.github.com>
2024-04-21 05:29:13 -0700

    created to_do_app

---
+++ index.ts
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+
+let todos: any = [];
+let condition = "true";
+
+while (condition)
+{
+    let answer = await inquirer.prompt([{
+
+    name: "task",
+    type: "input",
+    message: "what is the name of your list?",
+
+},
+{
+    name : "todo",
+    type: "input",
+      message: "what do you want to include in it?",
+}
+
+]);
+todos.push(answer.task);
+todos.push(answer.todo);
+
+let addtask = await inquirer.prompt([
+    {
+    name: "more",
+    type: "confirm",
+    message: "Do you want to add somthing",
+    default: false,
+}
+]);
+
+condition = (addtask.more);
+ console.log("your todo list is:" + todos);
+
+
+}
+ 
